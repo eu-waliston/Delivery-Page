ddb684cd2a981ec06abf0270cac8eaaf8b853358
Waliston E. Santos <82295321+bkt-tonton@users.noreply.github.com>
2022-03-19 13:31:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  stringsElement:'#Company-name',
+  strings: ['<h1>Well Fast Delivery <br> Your delivery is safe with us </h1>', '&amp; <br> <h1>Security and commitment for over 15 years. </h1> <br> <h1>Entre em contato com a gente, entregamos em qualquer lugar do pais<h1><br>'],
+  typeSpeed: 40
+};
+
+
+
+
